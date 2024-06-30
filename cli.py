07b61a85dcc89c3cdce4ed57bbc31ddcec572b4b
tsouchlarakis <andoni.sooklaris@gmail.com>
2024-06-30 15:57:26 -0400

    Fix imports with new package structure following #7

--- cli.py
+++ cli.py
@@ -1,7 +1,9 @@
 import click
-from .src.scrape_conjugations import scrape_conjugations
-from .src.show import show
-from .src.anki_flashcard import anki_flashcard
+
+from greek_utils.src.anki_flashcard import anki_flashcard
+from greek_utils.src.mantinades_flashcard import mantinades_flashcard
+from greek_utils.src.scrape_conjugations import scrape_conjugations
+from greek_utils.src.show import show
 
 
 @click.group()
@@ -15,6 +17,7 @@ def cli():
 cli.add_command(scrape_conjugations)
 cli.add_command(show)
 cli.add_command(anki_flashcard)
+cli.add_command(mantinades_flashcard)
 
 
 def main(args=None):
